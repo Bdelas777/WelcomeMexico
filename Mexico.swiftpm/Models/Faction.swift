a5ae3460ee2409e3c242f92d20374f423bc05a88
danzosr <93608793+Bdelas777@users.noreply.github.com>
2025-01-24 19:03:07 -0600

    update info

--- Mexico.swiftpm/Models/Faction.swift
+++ Mexico.swiftpm/Models/Faction.swift
@@ -17,29 +17,30 @@ struct Faction: Identifiable {
 }
 
 // Lista de facciones
+// Faction list
 let factions = [
     Faction(
         name: "Olmecs",
         imageName: "olmec",
-        description: "Known as the 'Mother Culture' of Mesoamerica. Famous for their colossal heads carved from basalt.",
+        description: "Uncover the origins of Mesoamerican civilization! The Olmecs, known as the 'Mother Culture,' invite you into a world of mystery and monumental art. Can you unravel the secrets behind their colossal stone heads?",
         destination: AnyView(OlmecGameView())
     ),
     Faction(
         name: "Teotihuacán",
         imageName: "teo",
-        description: "A massive city and religious center. Famous for its pyramids, including the Pyramid of the Sun.",
+        description: "Step into the majestic City of the Gods. Climb the towering Pyramid of the Sun and uncover the mysteries of Teotihuacán, where religion, science, and power collide. Are you ready to walk among giants?",
         destination: AnyView(JuegoPiramideView())
     ),
     Faction(
         name: "Tlaxcaltecas",
         imageName: "tlax",
-        description: "Allied with Hernán Cortés to defeat the Mexicas/Aztecs. Key players in the Spanish conquest.",
+        description: "Become a key strategist in shaping Mesoamerica's destiny. Join the Tlaxcaltecas, daring warriors who forged alliances that changed history forever. Will you dare to face the mighty Mexicas?",
         destination: AnyView(GameView())
     ),
     Faction(
         name: "Realistas",
         imageName: "goal",
-        description: "Loyalists who opposed Mexican independence. Played a crucial role during the War of Independence.",
+        description: "Experience the tension and drama of the fight for independence. Step into the shoes of the Realists, loyal to the Spanish crown, as they battle to preserve their cause. Can you hold your ground against the roar of revolution?",
         destination: AnyView(IndepeView())
     )
 ]
